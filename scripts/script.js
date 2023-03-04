3dba190e52af08db83f9535e60d404202f169116
Tanzeebul-Tamim <tamim200091@gmail.com>
2023-03-04 19:04:43 +0600

    pricing

--- scripts/script.js
+++ scripts/script.js
@@ -50,11 +50,40 @@ const displayCards = (cards, dataLimit) => {
                <h1 class="text-2xl font-bold mt-2 mb-2">${card.name}</h1>
                <h3 class="text-lg text-neutral-500"><i style="margin-right: 5px;" class="fa-regular fa-calendar"></i> ${card.published_in}</h3>
            </div>
-           <i id="btn-details" style="font-size: 25px;" class="fa-solid fa-arrow-right p-3 rounded-full text-rose-500 bg-red-50 hover:bg-red-100 hover:text-rose-600 cursor-pointer" data-bs-toggle="modal" data-bs-target="#cardDetailModal"></i>
+           <i onclick="loadCardDetails('${card.id}')" style="font-size: 25px;" class="fa-solid fa-arrow-right p-3 rounded-full text-rose-500 bg-red-50 hover:bg-red-100 hover:text-rose-600 cursor-pointer" data-bs-toggle="modal" data-bs-target="#cardDetailModal"></i>
         </div>
       `;
     cardsContainer.appendChild(cardDiv);
   });
 };
+
+const loadCardDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const res = await fetch (url);
+    const data = await res.json ();
+    displayCardDetails(data.data);
+    console.log(data.data)
+}
+
+const displayCardDetails = card => {
+    const modalTitle = document.getElementById('cardDetailModalLabel');
+    modalTitle.innerText = card.description;
+
+    const pricing = document.getElementById('pricing');
+    pricing.innerHTML = `
+    <div class="bg-purple-200 font-bold px-6 py-3 rounded-xl flex flex-col justify-center" style="color: #03A30A;">
+        <h3>${card.pricing[0].price === '0' || card.pricing[0].price === 'No cost' ? "Free of Cost" : card.pricing[0].price}</h3>
+        <h3>${card.pricing[0].price === '0' || card.pricing[0].price === 'No cost' ? "" : card.pricing[0].plan}</h3>
+    </div>
+    <div class="bg-purple-200 font-bold px-6 py-3 rounded-xl flex flex-col justify-center" style="color: #F28927">
+        <h3>${card.pricing[1].price === '0' || card.pricing[1].price === 'No cost' ? "Free of Cost" : card.pricing[1].price}</h3>
+        <h3>${card.pricing[1].price === '0' || card.pricing[1].price === 'No cost' ? "" : card.pricing[1].plan}</h3>
+    </div>
+    <div class="bg-purple-200 font-bold px-6 py-3 rounded-xl flex flex-col justify-center" style="color: #EB5757">
+        <h3>${card.pricing[2].price}</h3>
+        <h3>${card.pricing[2].plan}</h3>
+    </div>
+    `;
+}
 
 loadCards(6);
